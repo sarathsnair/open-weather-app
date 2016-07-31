c3a64794e6e18935156332d4f8f2dafbacd057ad
Sarath S Nair <Sarath S Nair>
2016-07-31 19:18:34 +0530

    weatherService is updated to get current-weather as well as daily-forcast

--- websrc/app/services/weatherService.js
+++ websrc/app/services/weatherService.js
@@ -2,10 +2,19 @@ define([], function () {
     var weatherService = function ($resource) {
         var apiKey = '9519611ad4c0e54e0e460a4b9ae2b2ca';
         var apiBaseUrl = "http://api.openweathermap.org/data/2.5/";
-        apiBaseUrl += "weather?appid="+apiKey;
+        apiBaseUrl += ':path/:subPath?q=:location';
         return $resource(apiBaseUrl,
-            {},
-            { 'getForecastByLocation': { method: 'GET', isArray: false } });
+            {
+                APPID: apiKey,
+                mode: 'json',
+                callback: 'JSON_CALLBACK',
+                units: 'metric',
+                lang: 'en'
+            },
+            {
+                'getWeatherByLocation': { method: 'JSONP', params: { path: 'weather' }, isArray: false },
+                'getForecastDaily': { method: 'JSONP', params: { path: 'forecast', subPath: 'daily', cnt: 7 }, isArray: false }
+            });
     };
     weatherService.$inject = ["$resource"];
     return weatherService;
