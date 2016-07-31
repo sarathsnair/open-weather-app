f332b4baadd0383852d079c2ae2aaea428af3ffa
Sarath S Nair <Sarath S Nair>
2016-07-31 21:25:57 +0530

    Rmoved console log

--- websrc/app/controllers/weatherController.js
+++ websrc/app/controllers/weatherController.js
@@ -1,26 +1,20 @@
 define([],
     function () {
         var weatherController = function ($scope, weatherService) {
-            this.pageTitle = "open Weather Application";
-            this.searchPlaceHolder = "Enter a city name..";
-            this.location = "cochin";
-            this.forecast;
+            this.location = "";
+            this.forecast = null;
             this.getForecastByLocation = function (location) {
                 if (location === '' || location === undefined) {
                     return;
                 }
 
                 this.forecast = weatherService.getForecastDaily({ location: location });
-                console.log(this.forecast);
             };
-            //this.getForecastByLocation(this.location);
-
             this.parseDate = function (unixTimestamp) {
                 return new Date(unixTimestamp * 1000);
             };
 
             this.getIconImgUrl = function (iconName) {
-                console.log(iconName);
                 return (iconName ? 'http://openweathermap.org/img/w/' + iconName + '.png' : '');
             };
         };
